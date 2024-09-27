44ede1c28aeeedd09b134e5b6025d13aa3f2a703
vendhan30 <vendhanazhagu@gmail.com>
2024-09-27 09:45:05 +0530

    yes commit done

--- src/main/java/com/Devops/Devopscontroller.java
+++ src/main/java/com/Devops/Devopscontroller.java
@@ -1,5 +1,15 @@
 package com.Devops;
 
-public class Devopscontroller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-}
+@RestController
+public class Devopscontroller {
+		@GetMapping(value="/get")	
+		public String getthis() {
+			return "hello";
+		}
+
+	}
+
+
